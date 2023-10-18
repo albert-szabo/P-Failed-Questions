f1be9263b6ef8204f0706a8ad6526b452574ea2e
Albert Szabo <45501122+albert-szabo@users.noreply.github.com>
2023-10-17 20:31:28 -0700

    Added loop with correct logic to prevent double counting

--- One.js
+++ One.js
@@ -6,4 +6,15 @@ const one = (prices, discount) => {
     let total = 0;
 
     const mostExpensive = Math.max(...prices);
+
+    for (const price of prices) {
+        if (price === mostExpensive) {
+            const newPrice = price * (1 - (discount / 100));
+            total += newPrice;
+        } else {
+            total += price;
+        }
+    }
+
+    return total;
 }
