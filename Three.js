521d33ffa17a52b9654cd98a158e00d8610bda04
Albert Szabo <45501122+albert-szabo@users.noreply.github.com>
2023-10-17 21:11:43 -0700

    Added logic & recursion

--- Three.js
+++ Three.js
@@ -5,5 +5,10 @@ const sampleToIDs = [3, 3, 4, 6, 9, 5, 6];
 const three = (start, fromIDs, toIDs) => {
     let startIndex = fromIDs.indexOf(start);
 
-    
+    if (startIndex === -1) {
+        return start;
+    } else {
+        start = toIDs[startIndex];
+        return three(start, fromIDs, toIDs);
+    }
 }
