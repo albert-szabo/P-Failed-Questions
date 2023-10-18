9f869442719204098cab106ac066ae58f001a4c0
Albert Szabo <45501122+albert-szabo@users.noreply.github.com>
2023-10-17 21:10:13 -0700

    Added function scaffolding & starter variable

--- Three.js
+++ Three.js
@@ -1,3 +1,9 @@
 const sampleFromIDs = [7, 1, 3, 4, 6, 9, 2];
 
-const sampleToIDs = [3, 3, 4, 6, 9, 5, 6]; 
+const sampleToIDs = [3, 3, 4, 6, 9, 5, 6];
+
+const three = (start, fromIDs, toIDs) => {
+    let startIndex = fromIDs.indexOf(start);
+
+    
+}
